73d6ae866aba70056b5d0e6c85ab03cc29e79066
Swabhan Katkoori <swabhankatkoori@gmail.com>
2025-04-09 00:10:09 -0400

    Inital Build System

---
+++ test_lib/main.cpp
@@ -0,0 +1,15 @@
+/**
+ * @file main.cpp
+ * @brief Test file for Synolo Library
+ * @version 0.1
+ * 
+ * This file allows us to test our Synolo Library
+ */
+
+#include "filter.hpp"
+
+int main() {
+    Filter test;
+    test.hello_world();
+    return 0;
+}
